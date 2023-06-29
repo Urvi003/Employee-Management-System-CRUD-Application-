131b0098e0a1533d3f675b5df81158527a6f24fa
Urvi003 <130202962+Urvi003@users.noreply.github.com>
2023-06-29 15:32:08 +0530

    Add files via upload

---
+++ Employee_Management_System/src/Employee_Management_System/Conn.java
@@ -0,0 +1,16 @@
+package Employee_Management_System;
+import java.sql.*;
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem", "root", "urvipatel");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
